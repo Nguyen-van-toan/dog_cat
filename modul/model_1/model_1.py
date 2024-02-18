d42d4ef175d385e1eaa87deec01522f943ceccbb
Nguyen-van-toan <toanvippk115@gmail.com>
2024-02-18 13:25:28 +0700

    upload 2 model

---
+++ modul/model_1/model_1.py
@@ -0,0 +1,50 @@
+import os
+import cv2
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+
+class ImageClassifier:
+    def __init__(self, dataset_path, image_size=(100, 100), test_size=0.2, random_state=42):
+        self.dataset_path = dataset_path
+        self.image_size = image_size
+        self.test_size = test_size
+        self.random_state = random_state
+        self.data = []
+        self.labels = []
+        self.classifier = None
+
+    def extract_features(self, image_path):
+        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        resized_image = cv2.resize(image, self.image_size)
+        return resized_image.flatten()
+
+    def load_data(self):
+        for category in os.listdir(self.dataset_path):
+            category_path = os.path.join(self.dataset_path, category)
+            for image_name in os.listdir(category_path):
+                image_path = os.path.join(category_path, image_name)
+                self.data.append(self.extract_features(image_path))
+                self.labels.append(category)
+        self.data = np.array(self.data)
+        self.labels = np.array(self.labels)
+
+    def train(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.data, self.labels, test_size=self.test_size, random_state=self.random_state)
+        self.classifier = RandomForestClassifier(n_estimators=100, random_state=self.random_state)
+        self.classifier.fit(X_train, y_train)
+        predictions = self.classifier.predict(X_test)
+        accuracy = accuracy_score(y_test, predictions)
+        return accuracy
+
+    def predict(self, image_path):
+        if self.classifier is None:
+            print("Classifier not trained yet.")
+            return None
+        features = self.extract_features(image_path)
+        prediction = self.classifier.predict([features])
+        return prediction[0]
+
+
---
+++ modul/model_2/main.py
@@ -0,0 +1,29 @@
+# import libaries
+import os
+import cv2
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+# import model
+from model_2 import ImageClassifier
+
+if __name__ == "__main__":
+    classifier = ImageClassifier(dataset_path=r"E:\learn\python\learn_ml\dog_cat\input\train_frames\train")
+    classifier.load_data()
+    features = classifier.extract_features(r"E:\learn\python\learn_ml\dog_cat\input\train_frames\train\cat\cat009.png")
+    image_matrix = features.reshape(classifier.image_size) 
+    # Display image
+    plt.imshow(image_matrix, cmap='gray')
+    plt.axis('off')  # Tắt trục
+    plt.show()
+    
+    
+    accuracy = classifier.train()
+    print("Accuracy:", accuracy)
+    # Example of using the trained classifier to predict a single image
+    image_path_to_predict = r"E:\learn\python\learn_ml\dog_cat\input\train_frames\train\cat\cat007.png"
+    prediction = classifier.predict(image_path_to_predict)
+    print("Prediction for", image_path_to_predict, ":", prediction)
+    
